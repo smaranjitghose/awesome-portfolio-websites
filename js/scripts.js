058f61fe969b3e823ba059b4105c84edeeb95f76
adyasha <adyashamohanty8104@gmail.com>
2020-05-21 12:43:50 +0530

    Parallax Tilt Card

--- js/scripts.js
+++ js/scripts.js
@@ -36,4 +36,109 @@ particlesJS('particles-js', {
 	},
 	retina_detect: true
 });
-console.log('hi');
+
+
+/* Card designing*/
+
+class parallaxTiltEffect {
+
+	constructor({element, tiltEffect}) {
+  
+	  this.element = element;
+	  this.container = this.element.querySelector(".container");
+	  this.size = [300, 360];
+	  [this.w, this.h] = this.size;
+  
+	  this.tiltEffect = tiltEffect;
+  
+	  this.mouseOnComponent = false;
+  
+	  this.handleMouseMove = this.handleMouseMove.bind(this);
+	  this.handleMouseEnter = this.handleMouseEnter.bind(this);
+	  this.handleMouseLeave = this.handleMouseLeave.bind(this);
+	  this.defaultStates = this.defaultStates.bind(this);
+	  this.setProperty = this.setProperty.bind(this);
+	  this.init = this.init.bind(this);
+  
+	  this.init();
+	}
+  
+	handleMouseMove(event) {
+	  const {offsetX, offsetY} = event;
+  
+	  let X;
+	  let Y;
+  
+  
+		X = (-(offsetX - (this.w/2)) / 3) / 3;
+		Y = ((offsetY - (this.h/2)) / 3) / 3;
+	  
+  
+	  this.setProperty('--rY', X.toFixed(2));
+	  this.setProperty('--rX', Y.toFixed(2));
+  
+	  this.setProperty('--bY', (80 - (X/4).toFixed(2)) + '%');
+	  this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');
+	}
+  
+	handleMouseEnter() {
+	  this.mouseOnComponent = true;
+	  this.container.classList.add("container--active");
+	}
+  
+	handleMouseLeave() {
+	  this.mouseOnComponent = false;
+	  this.defaultStates();
+	}
+  
+	defaultStates() {
+	  this.container.classList.remove("container--active");
+	  this.setProperty('--rY', 0);
+	  this.setProperty('--rX', 0);
+	  this.setProperty('--bY', '80%');
+	  this.setProperty('--bX', '50%');
+	}
+  
+	setProperty(p, v) {
+	  return this.container.style.setProperty(p, v);
+	}
+  
+	init() {
+	  this.element.addEventListener('mousemove', this.handleMouseMove);
+	  this.element.addEventListener('mouseenter', this.handleMouseEnter);
+	  this.element.addEventListener('mouseleave', this.handleMouseLeave);
+	}
+  
+  }
+  
+  const $ = e => document.querySelector(e);
+  
+  const wrap1 = new parallaxTiltEffect({
+	element: $('.section1'),
+	tiltEffect: 'normal'
+  });
+
+  const wrap2 = new parallaxTiltEffect({
+	element: $('.section2'),
+	tiltEffect: 'normal'
+  });
+
+  const wrap3 = new parallaxTiltEffect({
+	element: $('.section3'),
+	tiltEffect: 'normal'
+  });
+
+  const wrap4 = new parallaxTiltEffect({
+	element: $('.section4'),
+	tiltEffect: 'normal'
+  });
+
+  const wrap5 = new parallaxTiltEffect({
+	element: $('.section5'),
+	tiltEffect: 'normal'
+  });
+
+  const wrap6 = new parallaxTiltEffect({
+	element: $('.section6'),
+	tiltEffect: 'normal'
+  });
