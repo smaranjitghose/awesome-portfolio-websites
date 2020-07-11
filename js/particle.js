264a6320180d9378de5145bd21cf8f26d955ce09
adyasha <adyashamohanty8104@gmail.com>
2020-07-12 00:02:50 +0530

    colors added in particle.js

--- js/particle.js
+++ js/particle.js
@@ -1,14 +1,14 @@
 particlesJS('particles-js', {
 	particles: {
 		number: { value: 80, density: { enable: true, value_area: 800 } },
-		color: { value: '#ffffff' },
+		color: { value: ['#C00C57','#2CD600','#3AD0ED','#ECB528'] },
 		shape: {
 			type: 'circle',
 			stroke: { width: 0, color: '#000000' },
 			polygon: { nb_sides: 5 },
-			image: { src: 'img/github.svg', width: 80, height: 80 }
+			image: { src: 'img/github.svg', width: 1000, height: 1000 }
 		},
-		opacity: { value: 0.5, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
+		opacity: { value: 3, random: false, anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false } },
 		size: { value: 3, random: true, anim: { enable: false, speed: 40, size_min: 0.1, sync: false } },
 		line_linked: { enable: true, distance: 150, color: '#ffffff', opacity: 0.4, width: 1 },
 		move: {
