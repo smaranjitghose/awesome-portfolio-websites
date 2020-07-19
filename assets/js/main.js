dfab1c1fe5344a816bca151f24bee71b4c969157
shubhkhanna <khannashubh04@gmail.com>
2020-07-19 07:15:47 +0530

    fixed height

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,31 +11,39 @@ const moocscards = [
     title: "Data Science",
     cardImage: "assets/images/education-page/mooc.jpg",
     instructor: "- Alex Aklson",
+    url: "https://github.com",
   },
   {
     title: "Cryptography",
     cardImage: "assets/images/education-page/mooc.jpg",
     instructor: "- Saurabh Mukhopadhyay",
+    url: "",
   },
   {
     title: "Machine Learning",
     cardImage: "assets/images/education-page/mooc.jpg",
     instructor: "- Andrew Ng",
+    url: "",
   },
 ];
 
 const showCards = () => {
   let output = "";
   moocscards.forEach(
-    ({ title, cardImage, instructor }) =>
+    ({ title, cardImage, instructor, url }) =>
       (output += `        
         <div class="col-md-4 ">
             <div class="card mb-2" >
                <div class="content">
                   <div class="content-overlay"></div>
                     <img src=${cardImage} class="card-img-top content-image" height=180>     
-                  <div class="content-details fadeIn-top">
-                    <h4>Certificate</h4>
+                  <div class="content-details fadeIn-bottom">
+                    <button class="btn">
+                    <a href=${url} target="_blank">
+                    <i data-eva="github-outline" data-eva-animation="shake" data-eva-fill="black"></i>
+                    </a>
+                    </button>
+                    
                   </div>
                 </div>
                 <div class="card-body p-1">
