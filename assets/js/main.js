2e4d045395269a185b9260746daee8cb3dad8150
shubhkhanna <khannashubh04@gmail.com>
2020-07-21 16:40:46 +0530

    Added some enhancements

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,26 +11,26 @@ const moocscards = [
     title: "Data Science",
     cardImage: "assets/images/education-page/mooc.jpg",
     instructor: "- Alex Aklson",
-    url: "https://github.com",
+    moocLink: "#!",
   },
   {
     title: "Cryptography",
     cardImage: "assets/images/education-page/mooc.jpg",
     instructor: "- Saurabh Mukhopadhyay",
-    url: "",
+    moocLink: "#!",
   },
   {
     title: "Machine Learning",
     cardImage: "assets/images/education-page/mooc.jpg",
     instructor: "- Andrew Ng",
-    url: "",
+    moocLink: "#!",
   },
 ];
 
 const showCards = () => {
   let output = "";
   moocscards.forEach(
-    ({ title, cardImage, instructor, url }) =>
+    ({ title, cardImage, instructor,moocLink }) =>
       (output += `        
         <div class="col-md-4 ">
             <div class="card mb-2" >
@@ -38,16 +38,11 @@ const showCards = () => {
                   <div class="content-overlay"></div>
                     <img src=${cardImage} class="card-img-top content-image" height=180>     
                   <div class="content-details fadeIn-bottom">
-                    <button class="btn">
-                    <a href=${url} target="_blank">
-                    <i data-eva="github-outline" data-eva-animation="shake" data-eva-fill="black"></i>
-                    </a>
-                    </button>
-                    
+                    <a href="${moocLink}" target="_blank"><i class="fa fa-info-circle fa-2x" aria-hidden="true" style="color: white;"></i></a>                                   
                   </div>
                 </div>
                 <div class="card-body p-1">
-                    <h6 class="mt-0 text-center font-weight-bold" style="font-size: 20px;">${title}</h6>
+                    <h6 class="mt-0 pt-2 text-center font-weight-bold" style="font-size: 20px;">${title}</h6>
                     <h6 class="pt-0 text-muted text-center font-weight-bold" style="font-size: 18px;">${instructor}</h6>
                 </div>
             </div>
