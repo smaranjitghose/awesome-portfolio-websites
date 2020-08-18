3d43ed821e853ee18893e5fe773d3423727cc2c5
harshita mangla <manglaharshita@gmail.com>
2020-08-18 19:54:52 +0530

    improved footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,35 @@ let footer = $(`
             "Develop a passion for learning. If you do, you will never cease to grow."
           </p>
         </div>
+
+        <div class="social-icons-footer">
+
+        <a class="social-icon-footer twitter" href="" target="_blank" rel="author">
+            <i class="fab fa-twitter"></i>
+        </a>
+
+        <a class="social-icon-footer dribbble" href="" target="_blank" rel="author">
+            <i class="fab fa-dribbble"></i>
+        </a>
+
+        <a class="social-icon-footer linkedin" href="" target="_blank" rel="author">
+            <i class="fab fa-linkedin-in"></i>
+        </a>
+
+        <a class="social-icon-footer medium" href="" target="_blank" rel="author">
+            <i class="fab fa-medium-m"></i>
+        </a>
+
+        <a class="social-icon-footer kaggle" href="" target="_blank" rel="author">
+            <i class="fab fa-kaggle"></i>
+        </a>
+
+        <a class="social-icon-footer github" href="" target="_blank" rel="author">
+            <i class="fab fa-github"></i>
+        </a>
+
+    </div>
+
         <div class="empty-space xs-25 sm-25"></div>
       </div>
     </div>
