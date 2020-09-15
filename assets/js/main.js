531e42a5742cd5b962a72ad1f4c4651fbff49b0a
khushboojoshi3 <64426164+khushboojoshi3@users.noreply.github.com>
2020-09-15 08:37:19 +0530

    Update in position of scroll to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
 
 //Get the top button
 var mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 500px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
