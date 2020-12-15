28b8ed37b70feb690cf1b3fe15f78ef7eff01003
Vishal-raj-1 <rajputvishal33786@gmail.com>
2020-12-15 13:47:44 +0530

    Remove logo from footer for mobile view

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,7 +96,7 @@ let footer = $(`
     <div class="row">
       <div class="col-lg-7 col-md-6 pt-3" >
       <div class="col-sm-12 col-md-10 ml-lg-4">
-        <a class="webintern-footer-logo col-12" href="#">
+        <a class="webintern-footer-logo col-12 d-none d-md-inline-block" href="#">
           <center><img src="assets/images/John-Doe1.png"style="width: 220px;"/></center>
         </a>
         <div class="simple-text text-center dark padding-sm">
