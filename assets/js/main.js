fc0f6a0af698350ae12c7a1ff41fcaf9280ba06c
PinRathod <pinrth2015@gmail.com>
2020-12-22 00:01:05 +0530

    Project

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,7 @@ let header = $(`
         <li><a class="white-text" href="projects.html">Projects</a></li>
         <li><a class="white-text" href="research.html">Research</a></li>
         <li><a class="white-text" href="education.html">Education</a></li>
+        <li><a class="white-text" href="skill.html">Skill</a></li>
         <li><a class="white-text" href="index.html">Blog</a></li>
         <li><a class="white-text" href="https://docs.google.com/document/d/1-SX2Pz1rD9D1IS-r5PpCCb1JeihSM_BtnI5Y9uruQGc/edit?usp=sharing" target="_blank">My Resume</a></li>
         <!--TOGGLE FOR LIGHT/DARK MODE-->
@@ -68,6 +69,7 @@ let header = $(`
 <li><a href="projects.html">Projects</a></li>
 <li><a href="research.html">Research</a></li>
 <li><a href="education.html">Education</a></li>
+<li><a href="skill.html">Skill</a></li>
 <li><a href="index.html">Blog</a></li>
 <li><a href="https://docs.google.com/document/d/1-SX2Pz1rD9D1IS-r5PpCCb1JeihSM_BtnI5Y9uruQGc/edit?usp=sharing" target="_blank">My Resume</a></li>
 
