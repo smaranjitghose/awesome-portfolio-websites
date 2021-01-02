e273e3e52b9d8e77d2df7ef00536a1147f95ed82
Dipansh Parmar <dipansh@pop-os.localdomain>
2021-01-02 10:52:24 +0530

    added existence check

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,24 +6,27 @@ $(document).ready(function () {
 
 //Get the top button
 var mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 350 ||
-    document.documentElement.scrollTop > 350
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+
+if(mybutton) {
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 350 ||
+      document.documentElement.scrollTop > 350
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
   }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 }
 
 //Footer and Navbar
