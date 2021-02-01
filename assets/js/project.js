00530e2499d4573faf5ecdac29a65b9da301394c
Chaitanya31612 <guptachaitanya31@gmail.com>
2021-02-01 16:28:32 +0530

    Project_Card_Tag_Bug #513 #525 Removed unexpected comma

--- assets/js/project.js
+++ assets/js/project.js
@@ -146,7 +146,8 @@ const showCards = () => {
                     <span>${tag}</span>
                 </button>`
               )
-        )}
+      ).join("")
+      }
               </div>
         <ul class="menu-content"><br>
               <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
