0654df4e049fe606337dc2310bcbbfbb7036785c
Anush Bhatia <anushbhatia1234@gmail.com>
2021-02-16 01:16:37 +0530

    footer added

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Nav from './Nav';
-
+import Footer from './Footer'
 export default function Layout({ children }) {
     return (
         <div>
             <Nav />
             {children}
+            <Footer/>
         </div>
     )
 }
