9d14791aefa098301df9a9e5a7ab9c8550aeaade
Jagannath8 <64650848+Jagannath8@users.noreply.github.com>
2021-06-03 01:24:00 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -104,7 +104,7 @@ let footer = $(`
         <p class="tag" >"Develop a passion for learning. If you do, you will never cease to grow."
         </p>
           <div class="rounded-social-buttons tag">
-            <a class="social-button twitter" href="#" target="_blank">
+            <a class="social-button twitter" label="twitter" href="https://twitter.com/" target="">
             <!-- SVG code for twitter icon -->
             <svg class="twitter-icon-footer" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 500 500" enable-background="new 0 0 500 500"
@@ -161,7 +161,7 @@ let footer = $(`
                                    c117.9-117.9,82.9-167.7,82.9-167.7l-2.8-0.1C241.6,182.3,238.2,181.9,233.2,181.5z" />
             </svg>
             </a>
-           <a class="social-button linkedin" href="" target="_blank" >
+           <a class="social-button linkedin" label="linkedin" href="https://linkedin.com/" target="" >
            <!-- svg code for linkedin icon -->
               <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
                 stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
@@ -170,7 +170,7 @@ let footer = $(`
                 <circle cx="4" cy="4" r="2"></circle>
               </svg>
             </a>
-            <a class="social-button instagram" href="" target="_blank" >
+            <a class="social-button instagram" label="instagram" href="https://instagram.com/" target="" >
              <!-- svg code for instagram icon -->
               <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
                  style="enable-background:new 0 0 202.5 202.5;">
@@ -180,7 +180,7 @@ let footer = $(`
                  <path id="camera" class="st0" d="M79,60.5h44c10.5,0,19,8.5,19,19v44c0,10.5-8.5,19-19,19H79c-10.5,0-19-8.5-19-19v-44                                                                            C60,69,68.5,60.5,79,60.5z" />
               </svg>
             </a>      
-            <a class="social-button github" href="#" target="_blank">
+            <a class="social-button github" label="github" href="https://github.com/" target="">
             <!-- SVG code for Github icon -->
             <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
                 <!-- body -->
