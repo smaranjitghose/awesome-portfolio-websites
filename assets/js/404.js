ce21ccb7444ad5b55705187b3e3c65b1e5a3381d
Jigar Sable <jigarsable21@gmail.com>
2021-06-28 10:42:21 +0530

    404 ui update

---
+++ assets/js/404.js
@@ -0,0 +1,19 @@
+anime({
+    targets: ".row svg",
+    translateY: 10,
+    autoplay: true,
+    loop: true,
+    easing: "easeInOutSine",
+    direction: "alternate",
+});
+
+anime({
+    targets: "#zero",
+    translateX: 10,
+    autoplay: true,
+    loop: true,
+    easing: "easeInOutSine",
+    direction: "alternate",
+    scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+    rotateY: { value: "+=180", delay: 200 },
+});
