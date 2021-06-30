8af08552c6f10912f90644fde7056bdfab9f11db
Rituparno Ghosh <rituparnocs@gmail.com>
2021-06-30 18:50:01 +0530

    Hackathon Section added in Expereince Page

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -109,48 +109,123 @@ const showCards = () => {
 document.addEventListener("DOMContentLoaded", showCards);
 
 
-// Mentorship Card
+// Hackathon Section
 
 
-const mentorshipcards = document.querySelector(".mentorship-cards");
+const hackathonsection = document.querySelector(".hackathon-section");
 const mentor = [
   {
-    title: "HakinCode",
-    image: "assets/images/experience-page/hakin.png",
-    time: "06/2020 - 08/2020",
-    desp: "<li>It is an open source community where students and mentors can apply.</li><hr /><li>Ample amount of technologies and projects are there and we are given opportunity to work on them according to our interest and knowledge.</li>",
+    title: "The Uplift Project",
+    subtitle: "Metor",
+    image: "assets/images/experience-page/uplift.png",
+    desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
   },
   {
-    title: "Google Summer of Code",
-    image: "assets/images/experience-page/gsoc.png",
-    time: "03/2020 - 08/2020",
-    desp: "<li>Google Summer of Code is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
+    title: "ULHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/ulhacks.png",
+    desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
   },
+  {
+    title: "WaffleHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/wafflehacks.png",
+    desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
+  },
+  {
+    title: "Elevate Tech",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/elevate.png",
+    desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
+  },
+  {
+    title: "PitchTeen",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/pitchteen.png",
+    desp: "<li>PitchTeen 2.0 was a tech startup competition for female and non-binary communities in high school and college from all over the world with an aim to create a safe and encouraging place for women to help them explore tech and business fields.</li>",
+  },
+  {
+    title: "Hack-A-Solution",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/hackasolution.png",
+    desp: "Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
+  },
+  {
+    title: "UniGlobe Hacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/uniglobe.png",
+    desp: "UniGlobe Hacks 2.0 is an upcoming 4-day long hackathon run by high school students held during 28-31 July 2021.",
+  },
+  {
+    title: "AtlasHacks",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/atlas.png",
+    desp: "AtlasHacks II is a 3-day virtual hackathon that aims to foster creativity within hackers all around the world.",
+  },
+  {
+    title: "NeoHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/neo.png",
+    desp: "NeoHacks is an upcoming 48-hour hackathon taking place July 23-25, 2021 where hundreds of students from around the world will come together virtually to learn new skills, meet new friends, and build awesome projects. We provide beginner-friendly workshops, mentorship, fun games, and more.",
+  },
+  {
+    title: "Mission Inspired",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/mission.png",
+    desp: "InspireEd Hacks is a nationwide initiative organized by the student-for-student nonprofit, Mission InspirEd, challenging students ages 10-18 to synthesize their creativity and coding experience into a product beneficial to the educational community.",
+  },
+  {
+    title: "Hack3",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/hack3.png",
+    desp: "Hack3 2021 is an upcoming global online hackathon that aims to enable high school students to learn computer science, with a broader message of (everyone can code) targeting underrepresented demographics to improve diversity and inclusion of individuals in the computer technology space.",
+  },
+  {
+    title: "JITHack",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/jithack.png",
+    desp: "JITHack is an upcoming virtual design and development hackathon organized by the students of Jyothy Institute of Technology, Bengaluru on June 26, 2021, where participants have 24 hours to build something incredible.",
+  },
+  {
+    title: "Recess Hacks",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/recess.png",
+    desp: "Recess Hacks is an upcoming hackathon for high school students in the Greater Toronto Area that strives to encourage innovation among high school students and get involved in technology.",
+  },
+  {
+    title: "Citro Tech",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/citro.png",
+    desp: "Citro Hacks 2021 is an upcoming all-inclusive online hackathon organized by Citro Tech, a student led organization dedicated to empowering minorities in the tech field</li>",
+  },
+  {
+    title: "NHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/nhacks.png",
+    desp: "NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
+  }
+  
+
 ];
 
 const showCards3 = () => {
   let output = "";
   mentor.forEach(
-    ({ title, image, time, desp}) =>
-      (output += `        
-      <div class="column mentorshipCard"> 
-      <div class="card card2 mentorshipCardCover">
-        <img src="${image}" class="card-img-top" alt="..."  width="64" height="300">
-        <div class="information">
-        <div class="card-body">
-          <h5 class="card-title">${title}</h5>
-          <p class=""sub-title">${time}</p>
+    ({ title, image, subtitle, desp}) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
         </div>
-        <div class="more-information">
-        <ul class="list-group list-group-flush p-0 right-aligned">
-          <div class="list-group-item card2 disclaimer">${desp}</div>
-        </ul>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="#" class="blog-slider__button">Read More</a>   
         </div>
-        </div>
-      </div>
       </div>
       `)
   );
-  mentorshipcards.innerHTML = output;
+  hackathonsection.innerHTML = output;
 };
 document.addEventListener("DOMContentLoaded", showCards3);
