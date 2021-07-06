6c3d37705fc6bb63b7d7a5bc513c39f01430cf36
Supritha Ravishankar <supri161@gmail.com>
2021-07-06 15:13:11 +0530

    added svg quote left icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -108,7 +108,8 @@ let footer = $(`
   <div class="p-4">
     <div class="container-fluid quote-container">
       <div class="quotes"    style="text-align:center;">
-        <i class="fas fa-feather" style="color:white;"></i>
+      <!--SVG code for quote-left icon-->
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
       <p class="tag" >Develop a passion for learning. If you do, you will never cease to grow.</p>
     </div>
