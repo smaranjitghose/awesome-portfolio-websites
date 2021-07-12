5e8b32b727b552e0557a9a8210576fa9d29158de
Anita-ihuman <charlesanita403@gmail.com>
2021-07-12 07:18:18 +0100

    Imported the images right

--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import styles from "./Experience.css";
-import Gnome from "./../../assets/images/workex/gnome.svg";
+import Gnome from "../../assets/images/workex/gnome.svg";
 import Layer5 from "../../assets/images/workex/layer5-white.png";
-import Google from "./../../assets/images/workex/google.png";
-// AOS.init();
+import Google from "../../assets/images/workex/google.png";
 
 const Experience = () => {
   const exp = [
@@ -14,13 +13,7 @@ const Experience = () => {
       time: "(May, 2020 - present)",
       desp: [
         {
-          text: " Created a modern new interface that contributed to a 15% increase in new user adoption rates within one month of rollout",
-        },
-        {
-          text: "Contributed to rollout of upgraded DEFSafe platform, delivering the company’s first persistent, end-to-end data protection that allow users to control security protocols across multiple devices",
-        },
-        {
-          text: "Successfully installed Linux servers and virtualized environments using Docker, Hyper-V, and Amazon Web Services",
+          text: " Created a modern new interface that contributed to a 15% increase in new user adoption rates within one month of rollout.",
         },
       ],
     },
@@ -31,13 +24,7 @@ const Experience = () => {
       time: "(Mar - Aug, 2019)",
       desp: [
         {
-          text: "Supported Layer5’s testing and engineering processes",
-        },
-        {
-          text: "Heavy focus on full stack engineering with modules on both front and back end processes.",
-        },
-        {
-          text: "Efficiently deployed and integrated software engineered by team and updated integration/deployment scripts to improve continuous integration practices",
+          text: "Supported Layer5’s testing and engineering processes.",
         },
       ],
     },
@@ -50,22 +37,20 @@ const Experience = () => {
         {
           text: "Built innovative microservices and Web Services (incl. SOA/SOAP/REST/XML).",
         },
-        {
-          text: "Implemented new C# class library for the SQL server database access layer, and updated previous web page frameworks",
-        },
-        {
-          text: "Sliced designer compositions from PSD format and created interactive HTML5 web-simulations utilizing CSS3, Javascript, ReactJS, and plugins",
-        },
       ],
     },
   ];
   return (
     <>
-      <section className={styles.section}>
+      <section>
         <div>
           {exp.map((item, id) => (
             <div key={item.id} className="card card1">
-              <figure className="featured-image">{item.cardImage}</figure>
+              <img
+                src={item.cardImage}
+                className="featured-image"
+              />
+
               <article className="card-body">
                 <header>
                   <div className="title">
