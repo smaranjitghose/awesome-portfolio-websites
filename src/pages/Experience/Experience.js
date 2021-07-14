627f395304cc9747815eec9db6b238382bfd6a02
Anita-ihuman <charlesanita403@gmail.com>
2021-07-14 12:30:04 +0100

    Adjusted the responsiveness

--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -9,11 +9,18 @@ const Experience = () => {
     {
       title: "Software Development Intern",
       cardImage: Gnome,
-      place: "Gnome",
+      place: "Gnome Foundation",
       time: "(May, 2020 - present)",
       desp: [
         {
           text: " Created a modern new interface that contributed to a 15% increase in new user adoption rates within one month of rollout.",
+        },
+        {
+          text: " Completed maintenance on existing programs.",
+        },
+
+        {
+          text: " Redesigned the rendering engine from scratch, speeding up the stock’s loading time by 600%",
         },
       ],
     },
@@ -26,6 +33,12 @@ const Experience = () => {
         {
           text: "Supported Layer5’s testing and engineering processes.",
         },
+        {
+          text: "Heavy focus on full stack engineering with modules on both front and back end processes.",
+        },
+        {
+          text: "Confirmed that completed software met client requirements",
+        },
       ],
     },
     {
@@ -37,6 +50,12 @@ const Experience = () => {
         {
           text: "Built innovative microservices and Web Services (incl. SOA/SOAP/REST/XML).",
         },
+        {
+          text: " Worked closely with product team on scope of future projects and innovations.",
+        },
+        {
+          text: "Carried out continuous identification, measurement, and improvement of processes.",
+        },
       ],
     },
   ];
@@ -45,19 +64,22 @@ const Experience = () => {
       <section>
         <div>
           {exp.map((item, id) => (
-            <div key={item.id} className="card card1">
+            <div key={item.id} className="exp-card">
               <img
                 src={item.cardImage}
-                className="featured-image" alt="experience logo"
+                className="featured-image"
+                alt="experience logo"
               />
 
               <article className="card-body">
                 <header>
                   <div className="title">
-                    <h3>{item.title}</h3>
+                    <h3 className="header">{item.place}</h3>
                     <p className="meta">
-                      <span className="pre-heading">{item.place}</span>
-                      <span className="author">{item.time}</span>
+                      <span className="role">{item.title}</span>
+                      <div>
+                        <span className="duration">{item.time}</span>
+                      </div>
                     </p>
                   </div>
                   <ul className="list">
