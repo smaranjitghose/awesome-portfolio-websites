fab2dbfaf082fbbc87eb223a0fe06d6f907b59ac
Sourav Seal <sourav.seal1999@gmail.com>
2021-07-18 13:03:29 +0530

    hackathon_card

--- src/pages/Experience/Hackathon.js
+++ src/pages/Experience/Hackathon.js
@@ -1,50 +1,59 @@
 import React from 'react'
+import { mentor } from './HackathonData'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Mousewheel,
+  Pagination
+} from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Pagination, Mousewheel]);
+
+const params = {
+  // container: ".container",
+  pagination: ".swiper-pagination",
+  paginationClickable: true,
+  direction: "vertical",
+  mousewheel: true,
+};
 
 function Hackathon() {
-    return (
-        <div>
-            <div className="blog-slider">
-                <div className="blog-slider__wrp swiper-wrapper">
-                    <div className="blog-slider__item swiper-slide">
-                        <div className="blog-slider__img">
-                            {/* <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt=""> */}
-                        </div>
-                        <div className="blog-slider__content">
-                            <span className="blog-slider__code">26 December 2019</span>
-                            <div className="blog-slider__title">Lorem Ipsum Dolor</div>
-                            <div className="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
-                            <a href="#" className="blog-slider__button">READ MORE</a>
-                        </div>
-                    </div>
-                    <div className="blog-slider__item swiper-slide">
-                        <div className="blog-slider__img">
-                            {/* <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/jason-leung-798979-unsplash.jpg" alt=""> */}
-                        </div>
-                        <div className="blog-slider__content">
-                            <span className="blog-slider__code">26 December 2019</span>
-                            <div className="blog-slider__title">Lorem Ipsum Dolor2</div>
-                            <div className="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
-                            <a href="#" className="blog-slider__button">READ MORE</a>
-                        </div>
-                    </div>
+  return (
+    <div>
+      <h4 className="text-center heading1 mcard">Hackathon <span className="my_experience">Participation</span></h4>
 
-                    <div className="blog-slider__item swiper-slide">
-                        <div className="blog-slider__img">
-                            {/* <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759871/alessandro-capuzzi-799180-unsplash.jpg" alt=""> */}
-                        </div>
-                        <div className="blog-slider__content">
-                            <span className="blog-slider__code">26 December 2019</span>
-                            <div className="blog-slider__title">Lorem Ipsum Dolor</div>
-                            <div className="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi?</div>
-                            <a href="#" className="blog-slider__button">READ MORE</a>
-                        </div>
-                    </div>
+      <main>
+        <div className="blog-slider mb-5 mt-5">
+          <Swiper {...params} pagination={true} className=" blog-slider__wrp swiper-wrapper">
+            {
+              mentor.map((data) => (
+                <SwiperSlide key={data.title} className="blog-slider__item swiper-slide" >
+                  {/* <div className="blog-slider__item swiper-slide"> */}
+                  <div className="blog-slider__img">
+                    <img src="https://picsum.photos/200/300" alt={data.title}></img>
+                  </div>
+                  <div className="blog-slider__content">
+                    <div className="blog-slider__title">{data.title}</div>
+                    <span className="blog-slider__code">{data.subtitle}</span>
+                    <div className="blog-slider__text">{data.desp}</div>
+                    <a href="#" className="blog-slider__button">Read More</a>
+                  </div>
 
-                </div>
-                <div className="blog-slider__pagination"></div>
-            </div>
+                  {/* </div> */}
+                </SwiperSlide>
+              ))
+            }
+            <div class="blog-slider__pagination"></div>
+          </Swiper>
         </div>
-    )
+      </main>
+    </div>
+  )
 }
 
 export default Hackathon
