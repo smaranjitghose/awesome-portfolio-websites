8c98214b7d6e66337ab5cd549d90b2d8fdad00a4
Paritosh tripathi <tripathiparitosh935@gmail.com>
2021-11-07 11:33:19 +0530

    exprenice update

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,25 +6,18 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Artificial intelligence Intern",
+    cardImage: "assets/images/experience-page/download.png",
+    place: "THE ONE WORLD",
+    time: "(Nov, 2021 - present)",
+    desp: "<li>Worked on recomendation system.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
-  },
-  {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Machine Learning Intern",
+    cardImage: "assets/images/experience-page/Uff.jpeg",
+    place: "UFF India",
+    time: "(Aug-Nov, 2019)",
+    desp: "<li> Collected, cleaned and tagged the Data for recomendation system</li><li>Reaserched on various nlp models</li><li>leanred pandas, numpy, seaborn</li>",
   },
 ];
 
