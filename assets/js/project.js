c89f6da64955fbab81db38cedbcf4a8b3daf9ff3
Mahesh B <crazymahesh98@gmail.com>
2022-01-11 09:42:39 +0530

    Updates to fix bug in project page

--- assets/js/project.js
+++ assets/js/project.js
@@ -109,7 +109,7 @@ const projects = [
 const showCards = () => {
   let output = "";
   projects.forEach(
-    ({ title, cardImage, tags, Previewlink, Githublink }) => {
+    ({ title, cardImage,description, tags, Previewlink, Githublink }) => {
       (output += `       
         <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
@@ -121,6 +121,7 @@ const showCards = () => {
                 <h1 class="title"><a href="#">${title}</a></h1>
                 </div>
             <ul class="menu-content"><br>
+                  <li class="description">${description}</li>
                   <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
                   <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
                 </ul>
@@ -137,12 +138,13 @@ document.addEventListener("DOMContentLoaded", showCards);
 
 function myFunction() {
   // Declare variables
-  var input, button, i, skillcard, card, title;
+  var input, button, i, skillcard, card, title,description;
   input = document.getElementById("myInput").value;
   input = input.toUpperCase();
   skillcard = document.getElementsByClassName("skill-card");
   card = document.getElementsByClassName("card");
   title = document.getElementsByClassName("title");
+  description=document.getElementsByClassName("description");
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < button.length; i++) {
