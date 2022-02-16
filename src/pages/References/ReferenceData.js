72b49b9beff188a2855ebfaf54c336620394a39b
Kushal <kushalghosh9899@gmail.com>
2022-02-16 20:51:08 +0530

    Updated Stylings

--- src/pages/References/ReferenceData.js
+++ src/pages/References/ReferenceData.js
@@ -3,29 +3,36 @@ import img2 from '../../assets/images/references-page/anush.png';
 import img3 from '../../assets/images/references-page/arnab.jpg';
 
 const ReferenceData=[
+  {
+    sl1: 1,
+    name1 : "Smaranjit Ghose",
+        designation1 : "Designation",
+        image1 : img1,
+        message1 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque blandit vestibulum porta. Phasellus at pulvinar sapien. Nam sit amet tellus vitae libero malesuada ultrices. Etiam accumsan diam sed diam hendrerit pretium.",
+        
+
+        sl2:2,
+        name2 : "Anush Bhatia",
+        designation2 : "Designation", 
+        image2: img2,
+        message2 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque blandit vestibulum porta. Phasellus at pulvinar sapien. Nam sit amet tellus vitae libero malesuada ultrices. Etiam accumsan diam sed diam hendrerit pretium.",
+        
+        
+    },
+
     {
-        sl: 1,
-        name: "Smaranjit Ghose",
-        designation: "Intern Manager at Flipkart",
-        image: img1,
-        message:
-          "It was a pleasure to work with John, who has a lot of knowledge, experience, and creativity. During his time here as an intern, he made many suggestions and improvements to many of Flipkart’s products, like our product pages and web apps. We really enjoyed his passion for creating AMP pages and the positive attitude he brought to our team.",
-      },
-      {
-        sl: 2,
-        name: "Anush Bhatia",
-        designation: "Intern Manager at Flipkart",
-        image: img2,
-        message:
-          "It was a pleasure to work with John, who has a lot of knowledge, experience, and creativity. During his time here as an intern, he made many suggestions and improvements to many of Flipkart’s products, like our product pages and web apps. We really enjoyed his passion for creating AMP pages and the positive attitude he brought to our team.",
-      },
-      {
-        sl: 3,
-        name: "Arnab Dutta Purkayastha",
-        designation: "Intern Manager at Flipkart",
-        image: img3,
-        message:
-          "It was a pleasure to work with John, who has a lot of knowledge, experience, and creativity. During his time here as an intern, he made many suggestions and improvements to many of Flipkart’s products, like our product pages and web apps. We really enjoyed his passion for creating AMP pages and the positive attitude he brought to our team.",
-      },
-]
+        sl1:3,
+        name1 : "Santosh Kumar",
+        designation1 : "Designation",
+        image1 : img3,
+        message1 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque blandit vestibulum porta. Phasellus at pulvinar sapien. Nam sit amet tellus vitae libero malesuada ultrices. Etiam accumsan diam sed diam hendrerit pretium.",
+        
+
+        sl2:4,
+        name2 : "Arnab Dutta Purkayastha",
+        designation2 : "Designation",
+        image2 : img3,
+        message2 : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque blandit vestibulum porta. Phasellus at pulvinar sapien. Nam sit amet tellus vitae libero malesuada ultrices. Etiam accumsan diam sed diam hendrerit pretium.",
+  },
+];
 export default ReferenceData;
