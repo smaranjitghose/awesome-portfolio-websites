210537e99c3561ef5893d514ea9dc52123e7bdce
Tushar Jain <tusharjain.752001@gmail.com>
2022-02-18 22:56:50 +0530

    Add files via upload

---
+++ src/pages/Experience/openSource.js
@@ -0,0 +1,155 @@
+import img1 from '../../assets/images/opensource/1.jpg'
+import img2 from '../../assets/images/opensource/2.jpg'
+import img3 from '../../assets/images/opensource/3.jpg'
+import img4 from '../../assets/images/opensource/4.jpg'
+import uplift from '../../assets/images/opensource/upllift.png'
+import atlas from '../../assets/images/opensource/atlas.png'
+import citro from '../../assets/images/opensource/citro.png'
+import elevate from '../../assets/images/opensource/elevate-hack.png'
+import hack3 from '../../assets/images/opensource/hack3.png'
+import hackasol from '../../assets/images/opensource/hackasol.png'
+import jithack from '../../assets/images/opensource/jithack.png'
+import mission from '../../assets/images/opensource/mission.png'
+import neo from '../../assets/images/opensource/neo.png'
+import nhack from '../../assets/images/opensource/nhack.png'
+import pitchteen from '../../assets/images/opensource/pitchteen.png'
+import recess from '../../assets/images/opensource/recess.png'
+import ulhack from '../../assets/images/opensource/ul-hack.png'
+import uniglobe from '../../assets/images/opensource/uniglobe.png'
+import waffle from '../../assets/images/opensource/waffle-hack.png'
+
+
+export const open = [
+    {
+        id:1,
+        title:"GirlScript Summer Of Code 2020",
+        content:"Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+        img:img1,
+    },
+    {
+        id:2,
+        title:"StudentCode-in 2020",
+        content:"Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
+        img:img2,
+    },
+    {
+        id:3,
+        title:"PClub Summer Of Code 2020",
+        content:"Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
+        img:img3,
+    },
+    {
+        id:4,
+        title:"Hakin-Codes",
+        content:"Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen",
+        img:img4,
+    },
+]
+export const hack = [
+    {
+        id:1,
+        title:"The Uplift Project",
+        badge:"Mentor",
+        content:"I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
+        img:uplift,
+    },
+    {
+        id:2,
+        title:"ULHacks",
+        badge:"Judge",
+        content:"ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
+        img:ulhack,
+    },
+    {
+        id:3,
+        title:"WaffleHacks",
+        badge:"Judge",
+        content:"WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
+        img:waffle,
+    },
+    {
+        id:4,
+        title:"Elevate Tech",
+        badge:"Judge",
+        content:"Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
+        img:elevate,
+    },
+    {
+        id:5,
+        title:"PitchTeen",
+        badge:"Judge",
+        content:"PitchTeen 2.0 was a tech startup competition for female and non-binary communities in high school and college from all over the world with an aim to create a safe and encouraging place for women to help them explore tech and business fields.",
+        img:pitchteen,
+    },
+    {
+        id:6,
+        title:"Hack-A-Solution",
+        badge:"Judge",
+        content:"Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
+        img:hackasol,
+    },
+    {
+        id:7,
+        title:"UniGlobe Hacks",
+        badge:"UniGlobe Hacks",
+        content:"UniGlobe Hacks 2.0 is an upcoming 4-day long hackathon run by high school students held during 28-31 July 2021.",
+        img:uniglobe,
+    },
+    {
+        id:8,
+        title:"AtlasHacks",
+        badge:"Mentor",
+        content:"AtlasHacks II is a 3-day virtual hackathon that aims to foster creativity within hackers all around the world.",
+        img:atlas,
+    },
+    {
+        id:9,
+        title:"NeoHacks",
+        badge:"Judge",
+        content:"NeoHacks is an upcoming 48-hour hackathon taking place July 23-25, 2021 where hundreds of students from around the world will come together virtually to learn new skills, meet new friends, and build awesome projects. We provide beginner-friendly workshops, mentorship, fun games, and more.",
+        img:neo,
+    },
+    {
+        id:10,
+        title:"Mission Inspired",
+        badge:"Judge",
+        content:"InspireEd Hacks is a nationwide initiative organized by the student-for-student nonprofit, Mission InspirEd, challenging students ages 10-18 to synthesize their creativity and coding experience into a product beneficial to the educational community.",
+        img:mission,
+    },
+    {
+        id:11,
+        title:"Hack3",
+        badge:"Judge",
+        content:"Hack3 2021 is an upcoming global online hackathon that aims to enable high school students to learn computer science, with a broader message of (everyone can code) targeting underrepresented demographics to improve diversity and inclusion of individuals in the computer technology space.",
+        img:hack3,
+    },
+    {
+        id:12,
+        title:"JITHack",
+        badge:"Mentor",
+        content:"JITHack is an upcoming virtual design and development hackathon organized by the students of Jyothy Institute of Technology, Bengaluru on June 26, 2021, where participants have 24 hours to build something incredible.",
+        img:jithack,
+    },
+    {
+        id:13,
+        title:"Recess Hacks",
+        badge:"Mentor",
+        content:"Recess Hacks is an upcoming hackathon for high school students in the Greater Toronto Area that strives to encourage innovation among high school students and get involved in technology.",
+        img:recess,
+    },
+    {
+        id:14,
+        title:"Citro Tech",
+        badge:"Mentor",
+        content:"Citro Hacks 2021 is an upcoming all-inclusive online hackathon organized by Citro Tech, a student led organization dedicated to empowering minorities in the tech field",
+        img:citro,
+    },
+    {
+        id:15,
+        title:"NHacks",
+        badge:"Judge",
+        content:"NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
+        img:nhack,
+    },
+    
+]
