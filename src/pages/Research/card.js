ea5a3cc0fea24013669e1fc51e0ee842e923af4c
Tanwi Kumari <22tanwikumari@gmail.com>
2022-03-09 01:26:18 +0530

    Update card.js

--- src/pages/Research/card.js
+++ src/pages/Research/card.js
@@ -1,15 +1,4 @@
-import React, { useState } from "react";
-import styles from "./Research.module.css";
-import inteferenceNetwork from "../../assets/images/research-page/inteferenceNetwork.png";
-import crossLingual from "../../assets/images/research-page/crossLingual.png";
-import wordRepresentation from "../../assets/images/research-page/wordRepresentation.png";
-import dialogueState from "../../assets/images/research-page/dialogueState.png";
-import semanticSegmentation from "../../assets/images/research-page/semanticSegmentation.png";
-import deepNetwork from "../../assets/images/research-page/deepNetwork.png";
-import imageDecomposition from "../../assets/images/research-page/imageDecomposition.png";
-import neuralNetworks from "../../assets/images/research-page/neuralNetworks.jpg";
-import { Helmet } from "react-helmet";
-import { propTypes } from "react-bootstrap/esm/Image";
+import React from "react";
 import Carddetails from "./cardetails";
 
 function Card(props) {
@@ -17,9 +6,10 @@ function Card(props) {
     <>
       {props.items.map((item) => (
         <Carddetails
-         
+          key={item.id}
           abstract={item.abstract_data}
           cite={item.cite_data}
+          image={item.src}
         />
       ))}
     </>
