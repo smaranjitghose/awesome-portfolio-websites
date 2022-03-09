d65b484b2654e7cd121db0975f638c965b7c39e6
Tanwi Kumari <22tanwikumari@gmail.com>
2022-03-09 23:04:26 +0530

    Update card.js

--- src/pages/Research/card.js
+++ src/pages/Research/card.js
@@ -1,17 +1,4 @@
 import React from "react";
-// import styles from "./Research.module.css";
-// import inteferenceNetwork from "../../assets/images/research-page/inteferenceNetwork.png";
-// import crossLingual from "../../assets/images/research-page/crossLingual.png";
-// import wordRepresentation from "../../assets/images/research-page/wordRepresentation.png";
-// import dialogueState from "../../assets/images/research-page/dialogueState.png";
-// import semanticSegmentation from "../../assets/images/research-page/semanticSegmentation.png";
-// import deepNetwork from "../../assets/images/research-page/deepNetwork.png";
-// import imageDecomposition from "../../assets/images/research-page/imageDecomposition.png";
-// import neuralNetworks from "../../assets/images/research-page/neuralNetworks.jpg";
-// import { Helmet } from "react-helmet";
-//import Carddetails from "./cardetails";
-// import { propTypes } from "react-bootstrap/esm/Image";
-//import Research from "./Research";
 import Carddetails from "./cardetails";
 
 function Card(props) {
@@ -22,6 +9,7 @@ function Card(props) {
           key={item.id}
           abstract={item.abstract_data}
           cite={item.cite_data}
+          image={item.src}
         />
       ))}
     </>
