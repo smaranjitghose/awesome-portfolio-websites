644f023ed3598ccf2948ac4160e474ac3dda35ec
William Zhao7140 <Chipotleguacamolly@gmail.com>
2023-08-30 22:34:59 -0700

    ch

--- assets/js/project.js
+++ assets/js/project.js
@@ -8,7 +8,7 @@ const projects = [
   {
     title: "Personalized Music Recommendation System",
     cardImage: "assets/images/project-page/music.jpg",
-    description: "This project leverages cosine similarity to create a tailored recommendation system for an individual's media preferences. By converting attributes (e.g, loudness, valence, speechiness) into vectors and employing cosine similarity, the system suggests items similar to the user's selected content. ",
+    description: "Built a personalized media recommendation system by converting track attributes into vectors and suggests items similar to user's choices",
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "",
@@ -17,7 +17,7 @@ const projects = [
   {
     title: "OpenAI Finetuned(High School Themed) Chatbot ",
     cardImage: "assets/images/project-page/openai-project.png",
-    description: "Created a custom OpenAI chatbot, finely tuned with a high school theme. Designed for my classmates, adding school-related references to make interactions more engaging.",
+    description: "Custom OpenAI chatbot tailored to engage classmates with school-related references.",
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "",
@@ -25,7 +25,7 @@ const projects = [
   {
     title: "Abalone EDA",
     cardImage: "assets/images/project-page/abalone-proj.jpg",
-    description: "I've explored alternative methods for accurately predicting the age of abalones, moving beyond the traditional ring-based approach. This involves considering factors like height, weight, and more to develop a more efficient and precise age prediction model.",
+    description: "Predicts abalone age using alternative methods considering factors like height and weight for a more accurate model.",
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "",
@@ -33,7 +33,7 @@ const projects = [
   {
     title: "Spam or Ham",
     cardImage: "assets/images/project-page/email-proj.png",
-    description: 'developed a spam email classifier using a machine learning model. This model is capable of distinguishing between "spam" and "ham" (legitimate) emails, effectively filtering out unwanted and potentially harmful content from users\' inboxes.',
+    description: 'Machine learning model to filter spam and ham emails, ensuring safer and cleaner inboxes.',
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
     Githublink: "",
@@ -43,28 +43,35 @@ const projects = [
 // function for rendering project cards data
 const showCards = () => {
   let output = "";
-  projects.forEach(({ title, cardImage, Previewlink, Githublink }) => {
-    output += `       
-        <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
-          <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
-            <div class="header">
-            </div>
-            <div class="data">
-              <div class="content">
+  projects.forEach(({ title, cardImage, Previewlink, Githublink, description }) => {
+    output += `
+      <div class="column skill-card card" style="margin: 15px" data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600">
+        <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
+          <div class="header"></div>
+          <div class="data">
+            <div class="content">
               <div class="title-div">
                 <h1 class="title"><a href="#">${title}</a></h1>
-                </div>
-            <ul class="menu-content"><br>
-                  <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
-                  <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
-                </ul>
+                <p>${description}</p>
+              
               </div>
+              
+              
             </div>
           </div>
-        </div>`;
+        </div>
+        <ul class="menu-content" style="position: absolute; bottom: 0; right: 0; margin: 10px;">
+          <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
+          <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
+        </ul>
+      </div>`;
   });
-  projectcards.innerHTML = output;
+  projectcards.innerHTML = output
 };
+
+
+
+
 document.addEventListener("DOMContentLoaded", showCards);
 
 function myFunction() {
@@ -90,3 +97,4 @@ function myFunction() {
     }
   }
 }
+
